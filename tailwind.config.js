e1ec6678dc14c9afd00d962b33b30d0159ea6931
Rizwanelansyah <rizwanelansyah55@gmail.com>
2024-03-04 11:31:55 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        back: "#F9D9AA",
+        "back-dark": "#FFA966",
+        coffee: "#2F2105",
+      },
+    },
+  },
+  plugins: [],
+};
